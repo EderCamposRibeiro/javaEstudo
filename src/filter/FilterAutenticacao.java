f1df8a740e91c22b010eeef23d408cb81c03827b
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-05-15 15:02:19 -0300

    Filter de autenticação de usuário - Parte 2

--- src/filter/FilterAutenticacao.java
+++ src/filter/FilterAutenticacao.java
@@ -1,16 +1,21 @@
-package excecoes;
+package filter;
 
 import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns={"/*"})
+import user.UserLogado;
+
+@WebFilter(urlPatterns={"/pages/acessoAoSistema.jsp"})
 public class FilterAutenticacao  implements Filter{
 	
 	// Faz alguma coisa quando a aplicação é derrubada
@@ -23,6 +28,18 @@ public class FilterAutenticacao  implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		// retorna null caso não esteja logado
+		UserLogado userLogado = (UserLogado) session.getAttribute("usuario");
+		
+		if (userLogado ==  null) { // usuário não logado
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/autenticar.jsp");
+			dispatcher.forward(request, response);
+			return; //interrompe o processo para redirecionar
+		}
 		
 		//executa as ações do request e response
 		chain.doFilter(request, response);
