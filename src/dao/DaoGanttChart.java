74e3240b8c80d35e5f94e1e5494f5cb8818bd2ff
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-06-01 16:24:47 -0300

    Criando um Gantt chart - Parte 3

---
+++ src/dao/DaoGanttChart.java
@@ -0,0 +1,60 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.Projeto;
+import beans.Series;
+import connections.ConnectionDataBase;
+
+public class DaoGanttChart {
+	
+	private Connection connection;
+	
+	public DaoGanttChart() {
+		connection = ConnectionDataBase.getConnection();
+	}
+	
+	public List<Projeto> getProjetos() throws Exception{
+		
+		List<Projeto> projetos = new ArrayList<Projeto>();
+		
+		String sqlProjetos = "select * from projeto";
+		PreparedStatement statementProjetos = connection.prepareStatement(sqlProjetos);
+		ResultSet resultSetProjetos = statementProjetos.executeQuery();
+		
+		while (resultSetProjetos.next()) {
+			Projeto projeto = new Projeto();
+			projeto.setId(resultSetProjetos.getLong("id"));
+			projeto.setNome(resultSetProjetos.getString("nome"));
+			
+			String sqlSeries = "select * from series where projeto = " + resultSetProjetos.getLong("id");
+			PreparedStatement statementSeries = connection.prepareStatement(sqlSeries);
+			ResultSet resultSetSeries = statementSeries.executeQuery();
+			
+			List<Series> series = new ArrayList<Series>();
+			
+			while (resultSetSeries.next()) {
+				Series serie = new Series();
+				serie.setId(resultSetSeries.getLong("id"));
+				serie.setNome(resultSetSeries.getString("nome"));
+				serie.setDatainicial(resultSetSeries.getString("datainicial"));
+				serie.setDatafinal(resultSetSeries.getString("datafinal"));
+				serie.setProjeto(resultSetSeries.getLong("projeto"));
+				
+				series.add(serie);
+			}
+			
+			projeto.setSeries(series);
+			
+			projetos.add(projeto);
+			
+		}
+		
+		return projetos;
+	}
+
+}
--- src/servlet/BuscarDatasPlanejamento.java
+++ src/servlet/BuscarDatasPlanejamento.java
@@ -1,31 +1,46 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import beans.Projeto;
+import dao.DaoGanttChart;
+
 @WebServlet("/pages/buscarDatasPlanejamento")
 public class BuscarDatasPlanejamento extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DaoGanttChart daoGanttChart = new DaoGanttChart();  
        
     public BuscarDatasPlanejamento() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String ganttJson = "["+
-				"{"+
-				"\"id\": \"1\", \"name\": \"Projeto Java Web\", \"series\": [" +
-				"{ \"name\": \"Planejado\", \"start\": new Date(2020,04,05), \"end\": new Date(2020,04,20) },"+
-				"{ \"name\": \"Real\", \"start\": new Date(2020,04,06), \"end\": new Date(2020,04,17), \"color\": \"#f0f0f0\" },"+
-				"{ \"name\": \"Projetado\", \"start\": new Date(2020,04,06), \"end\": new Date(2020,04,17), \"color\": \"#e0e0e0\" }"+
-			"]}]";
+		
+		try {
+			List<Projeto> projetos = daoGanttChart.getProjetos();
+			
+			if (!projetos.isEmpty()) {
+				
+				String ganttJson = new Gson().toJson(projetos);
 
-			response.setStatus(200);
-			response.getWriter().write(ganttJson);
+				response.setStatus(200);
+				response.getWriter().write(ganttJson);
+			}
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
